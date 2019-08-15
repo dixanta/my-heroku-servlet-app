17576da7d44e1edc26f7cb94ba1215177040a705
dixanta <dixanta@gmail.com>
2019-08-15 11:25:29 +0545

    servlet added

---
+++ src/main/java/com/cibt/webapp/servlet/HomeServlet.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cibt.webapp.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author apple
+ */
+@WebServlet(name = "homeServlet",urlPatterns = {"/","/home"})
+public class HomeServlet extends HttpServlet{
+
+    @Override
+    protected void doGet(HttpServletRequest request, 
+            HttpServletResponse response) throws ServletException, IOException {
+        
+        request.setAttribute("name", "Creators Institute");
+        request.getRequestDispatcher("/WEB-INF/views/home/index.jsp")
+                .forward(request, response);
+    }
+    
+}
---
+++ src/main/webapp/WEB-INF/views/home/index.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : index
+    Created on : Aug 15, 2019, 11:24:10 AM
+    Author     : apple
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello from ${name}!</h1>
+    </body>
+</html>
